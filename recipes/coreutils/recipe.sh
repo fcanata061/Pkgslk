ef64c7e820579fc7842695d91bc19e8bcae547c9
fcanata061 <fcanata61@gmail.com>
2025-09-06 09:08:15 -0300

    Update recipe.sh
    Adicionado opção para download com git

--- recipes/coreutils/recipe.sh
+++ recipes/coreutils/recipe.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 pkg_name="binutils"
 pkg_version="2.42"
@@ -6,17 +5,14 @@ pkg_url="https://ftp.gnu.org/gnu/binutils/binutils-$pkg_version.tar.xz"
 pkg_git=""
 pkg_deps=()
 pkg_desc="Coleção de utilitários GNU de baixo nível (binutils)"
+pkg_patches=()
+pkg_extra_sources=()
 
 build() {
     BUILD_SUBDIR="$1"
     mkdir -p "$BUILD_SUBDIR"
     cd "$BUILD_SUBDIR"
-
-    "$SRC_DIR/$pkg_name-$pkg_version/configure" \
-        --prefix="$BUILD_SUBDIR" \
-        --disable-nls \
-        --disable-werror
-
+    "$SRC_DIR/$pkg_name-$pkg_version/configure" --prefix="$BUILD_SUBDIR" --disable-nls --disable-werror
     make -j$(nproc)
     make install
 }
