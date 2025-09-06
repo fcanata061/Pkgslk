22146b4e97aee4d212715b8a6a4ca093e239f7c3
fcanata061 <fcanata61@gmail.com>
2025-09-06 09:03:00 -0300

    Update newpkg.sh
    Adicionado novas funcionalidades

--- newpkg.sh
+++ newpkg.sh
@@ -12,7 +12,7 @@ INSTALLED_DB="$DIR/packages/installed.db"
 mkdir -p "$(dirname "$INSTALLED_DB")"
 
 usage() {
-    echo "Uso: $0 {build|b|install|i|remove|r|list|l|info|search} <pacote>"
+    echo "Uso: $0 {build|b|install|i|remove|r|rebuild|rb|rebuildall|rba|prepare|p|clean|c|list|l|info|search} <pacote>"
     exit 1
 }
 
@@ -23,6 +23,10 @@ case "$cmd" in
     build|b) build_package "$pkg" ;;
     install|i) install_binary "$pkg" ;;
     remove|r) remove_binary "$pkg" ;;
+    rebuild|rb) rebuild_package "$pkg" ;;
+    rebuildall|rba) rebuild_system ;;
+    prepare|p) prepare_package "$pkg" ;;
+    clean|c) clean_all ;;
     list|l) list_recipes_with_status ;;
     info) show_info "$pkg" ;;
     search) search_package "$pkg" ;;
