a5102533003ffca97409087e968fc9f0e6dbc741
fcanata061 <fcanata61@gmail.com>
2025-09-06 08:46:57 -0300

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ SRC_DIR="$(pwd)/src"
 BUILD_DIR="$(pwd)/build"
 RECIPE_DIR="$(pwd)/recipes"
 PKG_DIR="$(pwd)/packages"
-mkdir -p "$PKG_DIR"
+mkdir -p "$SRC_DIR" "$BUILD_DIR" "$PKG_DIR"
 
 build_package() {
     local pkg="$1"
@@ -21,7 +21,7 @@ build_package() {
     mkdir -p "$SRC_DIR" "$BUILD_DIR/$pkg/files"
     cd "$SRC_DIR"
 
-    # Baixar ou atualizar fonte
+    # Baixar fonte
     if [[ ! -d "$SRC_DIR/$pkg_name-$pkg_version" ]]; then
         if [[ -n "$pkg_git" ]]; then
             git clone "$pkg_git" "$pkg_name-$pkg_version"
@@ -31,22 +31,23 @@ build_package() {
         fi
     fi
 
-    # Build dentro do subdir de build
+    # Build
     BUILD_SUBDIR="$BUILD_DIR/$pkg/files"
     mkdir -p "$BUILD_SUBDIR"
     cd "$SRC_DIR/$pkg_name-$pkg_version"
     build "$BUILD_SUBDIR"
 
-    # Criar metadata
+    # Metadata
+    mkdir -p "$BUILD_DIR/$pkg"
     echo "name=$pkg_name" > "$BUILD_DIR/$pkg/metadata"
     echo "version=$pkg_version" >> "$BUILD_DIR/$pkg/metadata"
     echo "deps=${pkg_deps[*]}" >> "$BUILD_DIR/$pkg/metadata"
+    [[ -n "$pkg_desc" ]] && echo "desc=$pkg_desc" >> "$BUILD_DIR/$pkg/metadata"
 
     # Empacotar com fakeroot
     cd "$BUILD_DIR/$pkg"
     PKG_FILE="$PKG_DIR/$pkg_name-$pkg_version.pkg.tar"
     info "Empacotando $pkg em $PKG_FILE"
     fakeroot tar -cf "$PKG_FILE" files metadata
-
     info "$pkg empacotado com sucesso!"
 }
